9bc5e547f37b46a008f281c27b555da9788efcb5
Geoff Maciolek <GeoffMaciolek@gmail.com>
2015-04-14 12:16:37 -0400

    Initial versions of bmailsearch.sh & assp-searcher.sh

---
+++ assp-searcher.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# ASSP Maillog searching tool v0.1
+# Geoff Maciolek
+# 2015-04-14 (older version), cleanup soon
+
+if [[ -z $1 ]]
+   then
+	echo
+	echo "ASSP Maillog search tool (by Geoff Maciolek)"
+	echo
+	echo "Usage:  $0 \"search string\" [days]"
+	echo
+	echo " \"days\" is optional, defaults to 3"
+	echo
+	echo "Examples:"
+	echo "   $0 \"The Mail Subject\" 5 - searches for 5 days worth of logs"
+	echo "   $0 \"username@domain\" - searches for 3 days worth of logs (default)"
+	echo
+	exit
+fi
+
+if [[ "" -eq $2 ]]
+  then
+    days="3"
+  else
+    days=$2
+fi
+
+echo "Here are the last $days days of log files"
+
+files=`find /usr/local/assp/logs/ -type f -name \*.txt -mtime -${days}`
+zfiles=`find /usr/local/assp/logs/ -type f -name \*.txt.gz -mtime -${days}`
+
+
+echo  "Searching non-compressed files..."
+egrep -i $1 $files
+
+if [[ -n ${zfiles} ]]
+   then
+      echo "Searching compressed files..."
+echo      zgrep -ie $1 $zfiles
+    else
+      echo "No compressed files."
+fi
+
+
+
